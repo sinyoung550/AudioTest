d117e8fc64b6ff00c8c37ef5aab20f9369f8c2c8
sinyoung550 <sinyoung550@gamil.com>
2017-06-19 19:18:00 +0900

    #3 Music ..

--- app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.parksinyoung.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+        Switch switchMusic;
+        MediaPlayer MusicPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchMusic=(Switch)findViewById(R.id.switch_music);
+        MusicPlayer = MediaPlayer.create(R.id.raw);
+        switchMusic.setOnClickListener(neme);
     }
 }
