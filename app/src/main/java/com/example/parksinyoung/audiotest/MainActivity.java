854f4057217a5ff8969e37a59e67e4d2e300f94e
sinyoung550 <sinyoung550@gamil.com>
2017-06-19 20:40:00 +0900

    #5 dasdf

--- app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
@@ -3,17 +3,34 @@ package com.example.parksinyoung.audiotest;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-        Switch switchMusic;
-        MediaPlayer MusicPlayer;
+        ListView list;
+    Button butPlay,butStop;
+    TextView textMusic;
+    ProgressBar progress;
+    String[] musics={"Rockabye","Shape Of You","Let Me Love You f Faded"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        switchMusic=(Switch)findViewById(R.id.switch_music);
-        MusicPlayer = MediaPlayer.create(R.id.raw);
-        switchMusic.setOnClickListener(neme);
+        list=(ListView)findViewById(R.id.List_music);
+        butPlay=(Button)findViewById(R.id.but_play);
+        butStop=(Button)findViewById(R.id.but_stop);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar)findViewById(R.id.progress_music);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice);
+        list.setAdapter(adapter);
+        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        list.setItemChecked(0,true);
+
     }
 }
