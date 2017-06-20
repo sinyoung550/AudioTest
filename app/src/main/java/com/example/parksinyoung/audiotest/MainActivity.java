3f26b1f82b71edaba90fa298c0e252444cf69daf
sinyoung550 <sinyoung550@gamil.com>
2017-06-20 19:57:30 +0900

    #5 pause

--- app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/audiotest/MainActivity.java
@@ -1,36 +1,85 @@
 package com.example.parksinyoung.audiotest;
 
+
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Switch;
 import android.widget.TextView;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-        ListView list;
-    Button butPlay,butStop;
+    ListView list;
+    Button butPlay,butStop,butpause;
     TextView textMusic;
     ProgressBar progress;
-    String[] musics={"Rockabye","Shape Of You","Let Me Love You f Faded"};
+    MediaPlayer mediaPlayer;
+    String[] musics ={"shape of you","faded","ddd"};
+    int[] musicResIds={R.raw.letmeloveyou,R.raw.rockabye,R.raw.shapeofyou};
+    int selectedMusicId;
+    boolean selectePauseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list=(ListView)findViewById(R.id.List_music);
+        list= (ListView)findViewById(R.id.List_music);
         butPlay=(Button)findViewById(R.id.but_play);
         butStop=(Button)findViewById(R.id.but_stop);
-        textMusic=(TextView)findViewById(R.id.text_music);
+        butpause=(Button)findViewById(R.id.but_pause);
         progress=(ProgressBar)findViewById(R.id.progress_music);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,musics);
         list.setAdapter(adapter);
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         list.setItemChecked(0,true);
+        selectedMusicId=musicResIds[0];
+        mediaPlayer= MediaPlayer.create(this,selectedMusicId);
 
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedMusicId=musicResIds[i];
+            }
+        });
+
+        butPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(selectePauseButton) {
+                    mediaPlayer.start();
+                    selectePauseButton=false;
+                } else{
+                    mediaPlayer=MediaPlayer.create(MainActivity.this,selectedMusicId);
+                }
+                mediaPlayer.start();
+                progress.setVisibility(view.VISIBLE);
+            }
+        });
+        butStop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                progress.setVisibility(View.INVISIBLE);
+            }
+        });
+        butpause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                selectePauseButton=true;
+                mediaPlayer.pause();
+                progress.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mediaPlayer.stop();
     }
 }
